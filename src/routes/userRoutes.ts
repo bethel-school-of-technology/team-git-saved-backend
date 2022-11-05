dd0ac84da60d4f3c98a49f89fbf832ce5895b267
Ford Henley <fordp86@gmail.com>
2022-11-05 17:00:49 -0500

    fixed routes then changed them back
    fixed routes then changed them back

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,21 +1,25 @@
-import {NextFunction, Request, Response, Router} from 'express';
-import { createUser, deleteUser, getAllUsers, getUser, loginUser, updateUser } from '../controllers/userController';
+import { NextFunction, Request, Response, Router } from "express";
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  loginUser,
+  updateUser,
+} from "../controllers/userController";
 
 const router = Router();
 
+router.get("/", getAllUsers);
 
+router.post("/", createUser);
 
-router.get('/', getAllUsers);
+router.get("/:id", getUser);
 
-router.post('/', createUser);
+router.put("/:id", updateUser);
 
-router.get('/:id', getUser);
+router.delete("/:id", deleteUser);
 
-router.put('/:id', updateUser);
-
-router.delete('/:id', deleteUser);
-
-router.post('/login', loginUser)
-
+router.post("/login", loginUser);
 
 export default router;
